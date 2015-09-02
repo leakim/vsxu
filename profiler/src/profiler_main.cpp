ea86090f5642d61039d45e2be5f6bad1899a8a2e
Mikael Wikstrom <leakim.wikstrom@gmail.com>
2015-09-02 16:35:07 +0200

    remove dead code, display_gpu_vram_stats is never used

--- profiler/src/profiler_main.cpp
+++ profiler/src/profiler_main.cpp
@@ -340,11 +340,13 @@ int main(int argc, char* argv[])
     }
   }
 
+#if 0
   int display_gpu_vram_stats = 0;
   if (vsx_argvector::get_instance()->has_param("gl_vram"))
   {
     display_gpu_vram_stats = 1;
   }
+#endif
 
 
   sprintf( titlestr, "Vovoid VSXu Profiler %s [GNU/Linux %d-bit]", vsxu_ver, PLATFORM_BITS);
